2ab870e338bc1d83bd0c2ae6c45d9117884d56ca
DosImpact <ypd03008@gmail.com>
2020-08-05 10:51:08 +0900

    example counter

--- main/src/index.ts
+++ main/src/index.ts
@@ -7,42 +7,57 @@ const chance = new Chance();
 
 let names: string[] = R.range(0, 3).map((e: number) => chance.name());
 
-console.log(names);
-
 let ages: number[] = R.range(0, 3).map((e: number) => chance.age());
-
-console.log(ages);
 
 let people: IPerson[] = R.range(0, 3).map((e: number) => ({
   name: chance.name(),
   age: chance.age(),
 }));
 
-// =========== example  split string    to string[]   : split
-// =========== example  split string[]  to string     : join
-
-const split = (str: string, delim: string = ""): string[] => str.split(delim);
-
-console.log(split("h,e,llo", ","));
-
-const join = (str: string[], delim: string = ""): string => str.join(delim);
-
-console.log(join(["hello", "world"], "|"));
-
-// =========== example  for...in | for...of |
-
-for (const idx in names) {
-  console.log(names[idx]);
+// make essential memory
+interface IMemory {
+  counter: number;
+  value: number;
 }
-
-for (const name of names) {
-  console.log(name);
+interface ICalFunc {
+  add: (arg0: number) => this;
+  sub: (arg0: number) => this;
+  getState: (arg0: number, arg1: number) => { counter: number; value: number };
+  resetState: () => boolean;
 }
-
-for (const [idx, name] of names.entries()) {
-  console.log(`[${idx}] : ${name}`);
+// make realize interface
+class ThisCal implements IMemory, ICalFunc {
+  static SERIRAL = "q1123f2w51sq3z5v8e741g681231q125";
+  constructor(public counter: number = 0, public value: number = 0) {
+    this.counter = counter;
+    this.value = value;
+  }
+  add(a: number) {
+    this.counter += 1;
+    this.value += a;
+    return this;
+  }
+  sub(a: number) {
+    this.counter += 1;
+    this.value -= a;
+    return this;
+  }
+  getState(): { counter: number; value: number } {
+    return {
+      counter: this.counter,
+      value: this.value,
+    };
+  }
+  resetState(): boolean {
+    this.counter = 0;
+    this.value = 0;
+    return true;
+  }
 }
-
-for (const person of people) {
-  console.log(person);
-}
+// test class
+let cal01: ThisCal = new ThisCal();
+console.log(ThisCal.SERIRAL);
+cal01.add(10).add(20).add(30);
+console.log(cal01.getState());
+cal01.resetState();
+console.log(cal01.getState());
