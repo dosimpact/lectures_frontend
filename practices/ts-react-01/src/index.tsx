18f25d44f7e099094312de4024f4c8c1239f8412
dosimpact <ypd03008@gmail.com>
2021-06-18 15:02:22 +0900

    webpack settings

---
+++ practices/ts-react-01/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>app23</div>;
+};
+// const Hot = hot(App);
+
+ReactDOM.render(<App />, document.querySelector("#root"));
--- practices/ts-react-01/webpack.config.js
+++ practices/ts-react-01/webpack.config.js
@@ -1,27 +1,35 @@
 const path = require("path");
 const webpack = require("webpack");
-
+// entry - loader - plugin - output
 module.exports = {
   mode: "development",
-  devtool: "eval", // hidden-source-map
+  devtool: "eval", // enum
   resolve: {
     extensions: [".jsx", ".js", ".tsx", ".ts"],
-  },
-
-  entry: {
-    app: "./client",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: "awesome-typescript-loader",
+        exclude: path.join(__dirname, "node_modules"),
       },
     ],
   },
   plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
+  entry: "./src/index",
   output: {
+    path: path.resolve(__dirname, "public"),
     filename: "app.js",
-    path: path.join(__dirname, "dist"),
+    publicPath: "/public",
+  },
+  // for webpack-dev-server
+  devServer: {
+    contentBase: __dirname + "/public/",
+    inline: true,
+    hot: true,
+    host: "localhost",
+    port: 3000,
+    open: true,
   },
 };
