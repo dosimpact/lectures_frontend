7307c1c5a7665f5e6e50edae570e19037a3a6944
dosimpact <ypd03008@gmail.com>
2021-07-09 21:56:40 +0900

    lazyloading

--- js-core/concepts/IntersectionObserver/03.IO/main.js
+++ js-core/concepts/IntersectionObserver/03.IO/main.js
@@ -18,14 +18,38 @@ const io = new IntersectionObserver((entries, observer) => {
     // entry와 observer 출력
     // 1. entry 관찰 대상의 El가 보이는지,교차되었는지
     console.log("entry:", entry);
-    console.log("observer:", observer);
     // 2. entry.isIntersecting 가 보이는 유무
     if (entry.isIntersecting) {
       console.log("is in viewport!");
     } else {
       console.log("is gone...:(");
     }
+    // 2.1 좌표 정보 ( getBoundClientRect 와 같은 정보를 반환)
+    // 순서대로 , 타겟의-A, root의-B=Viewport, 교차된 영역의(intersection element)(A 교 B)
+    // boundingClientRect: DOMRectReadOnly {x: 208, y: -157.5625, width: 300, height: 200, top: -157.5625, …}
+    // rootBounds: DOMRectReadOnly {x: -100, y: -100, width: 1022, height: 925, top: -100, …}
+    // intersectionRect: DOMRectReadOnly {x: 208, y: -100, width: 300, height: 142.4375, top: -100, …}
+
+    // 2.2 교차 정보
+    // isIntersecting: true
+    // 교차 여부
+    // intersectionRatio: 0.7121875286102295
+    // threshold 와 비슷 ( 0~1 )사이의 값으로 얼마나 교차되었는지에 대한 정보
+
+    // 2.3 그외 정보
+    // isVisible: false
+    // target: div.box
+    // 타켓 엘리먼트 객체 반환
+    // time: 76.5
+    // 교차가 기록된 시간 반환
   });
+  // 3. observer 객체(설정값이 나온다.)
+  console.log("observer:", observer);
+  // delay: 0
+  // root: null
+  // rootMargin: "100px 100px 100px 100px"
+  // thresholds: (3) [0, 0.5, 1]
+  // trackVisibility: false
 }, options);
 
 document.querySelectorAll(".box").forEach((node) => {
