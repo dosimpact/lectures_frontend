c05ce5c0ea60f9f2520c300c2683b0b2e4696383
DosImpact <ypd03008@gmail.com>
2021-08-18 16:20:38 +0900

    (feat) 20-strict

---
+++ js-docs-deepdive/20-strict/4-strict-mode-diff.js
@@ -0,0 +1,27 @@
+// 1. 일반함수의 this
+//     - 생성자 아니면 this는 undefined
+
+// (function () {
+//   "use strict";
+//   function foo() {
+//     console.log(this); // undefined
+//   }
+//   foo();
+//   function Bar() {
+//     console.log(this); // Bar {}
+//   }
+//   new Bar();
+// })();
+
+// 2. arguments 객체
+//     - 불변
+(function () {
+  //   "use strict";
+  function foo(a) {
+    a = 10;
+    console.log(arguments);
+    // [Arguments] { '0': 10 }
+    // [Arguments] { '0': 2 }
+  }
+  foo(2);
+})();
