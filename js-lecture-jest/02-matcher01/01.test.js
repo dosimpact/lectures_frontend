b4e5480909ee6a4c03256e3e8aae4566525a12cb
dosimpact <ypd03008@gmail.com>
2021-09-25 17:19:56 +0900

    (feat) jest 3,4

--- js-lecture-jest/02-matcher01/01.test.js
+++ js-lecture-jest/02-matcher01/01.test.js
@@ -21,7 +21,7 @@ describe("matcher 02 - object", () => {
   beforeAll(() => {
     goalUser = { name: "dodo", age: 20 };
   });
-  //✅ 객체를 비교할때는, 프러퍼티까지 같은지 봐야 하므로 , toEqual을 사용한다.
+  //✅ 객체를 비교할때는, 프로퍼티까지 같은지 봐야 하므로 , toEqual을 사용한다.
   // - 하지만 undefiend (초기선언상태)의 프로퍼티는 검사 제외
   test("object not tobe (diff addr)", () => {
     expect(fn.makeUser("dodo", 20)).not.toBe(goalUser);
@@ -36,5 +36,63 @@ describe("matcher 02 - object", () => {
   // ✅ undefined 프로퍼티까지 일치하는지 확인할땐 toStrictEqual
   test("object toEqual (deep equality)", () => {
     expect(fn.makeUser("dodo", 20)).not.toStrictEqual(goalUser);
+  });
+});
+
+// toBeNull
+// toBeUndefined
+// toBeDefined
+describe("matcher 03 ", () => {
+  test("null toBeNull", () => {
+    expect(null).toBeNull();
+  });
+  test("undefined toBeUndefined", () => {
+    expect(undefined).toBeUndefined();
+  });
+  test("defined toBeDefined", () => {
+    //✅ null 은 객체이고, 값에 정의가 되어져 있는 상태
+    expect(null).toBeDefined();
+    expect(undefined).not.toBeDefined();
+  });
+});
+
+// toBeTruthy
+// toBeFalsy
+describe("matcher 04 ", () => {
+  test("toBeTruthy", () => {
+    expect(true).toBeTruthy();
+    expect([]).toBeTruthy();
+    expect({}).toBeTruthy();
+  });
+  test("toBeTruthy", () => {
+    expect(false).toBeFalsy();
+    expect("").toBeFalsy();
+    expect(!!false).toBeFalsy();
+    expect(!!undefined).toBeFalsy();
+    expect(!!null).toBeFalsy();
+  });
+});
+
+// toBeGreaterThan
+// toBeGreaterThanOrEqual
+// toBeLessThan
+// toBeLessThanOrEqual
+// toBeCloseTo
+describe("matcher 05", () => {
+  test("10 > 3", () => {
+    expect(10).toBeGreaterThan(3);
+  });
+  test("10 >= 3", () => {
+    expect(10).toBeGreaterThanOrEqual(3);
+  });
+  test("3 < 10 ", () => {
+    expect(3).toBeLessThan(10);
+  });
+  test("3 <= 10 ", () => {
+    expect(3).toBeLessThanOrEqual(10);
+  });
+  test("3.1 + 7.0 ~~= 10.1", () => {
+    expect(0.1 + 0.2).not.toBe(0.3);
+    expect(0.1 + 0.2).toBeCloseTo(0.3);
   });
 });
--- js-lecture-jest/README.md
+++ js-lecture-jest/README.md
@@ -8,12 +8,3 @@ Jest 설치하기
 
 - npm i jest -D
 - script "test":"jest"
-
-## 02
-
-[ref][https://jestjs.io/docs/expect#tostrictequalvalue]
-
-- toStrictEqual
-- 1 e.g. {a: undefined, b: 2} does not match {b: 2}
-- 2 [, 1] does not match [undefined, 1]
-- 3 A class instance with fields a and b will not equal a literal object with fields a and b. ( obj key 가 다르다. )
