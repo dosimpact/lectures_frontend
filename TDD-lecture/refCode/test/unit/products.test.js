93be7cc6198007f20fe4f505925284f36772c71e
dosimpact <ypd03008@gmail.com>
2021-10-06 13:39:51 +0900

    (issue) unit recursive module

--- TDD-lecture/refCode/test/unit/products.test.js
+++ TDD-lecture/refCode/test/unit/products.test.js
@@ -26,23 +26,28 @@ describe("Product Controller Create", () => {
   beforeEach(() => {
     req.body = newProduct;
   });
+
   it("should have a createProduct funtcion", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
+
   it("should call ProductModel.create", async () => {
     await productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
   });
+
   it("should return 201 response code", async () => {
     await productController.createProduct(req, res, next);
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy();
   });
+
   it("should return json body in response", async () => {
     productModel.create.mockReturnValue(newProduct);
     await productController.createProduct(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newProduct);
   });
+
   it("should handle errors", async () => {
     const errorMessage = { message: "description property missing" };
     const rejectedPromise = Promise.reject(errorMessage);
