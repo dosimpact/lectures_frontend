9eb1ac86e522a2c551eaf777ccddb41b3de7923d
DosImpact <ypd03008@gmail.com>
2021-12-27 16:53:42 +0900

    react-webpack

---
+++ webpack-lecture/1_webpack/16_react_webpack/src/index.js
@@ -0,0 +1,8 @@
+require("file-loader?name=[name].[ext]!./index.html");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./App.scss";
+
+ReactDOM.render(<App />, document.getElementById("app"));
