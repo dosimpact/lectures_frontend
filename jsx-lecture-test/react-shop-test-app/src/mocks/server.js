aa7ec6da2cf15b75a3fc8e5f2ffc62820f56e545
dosimpact <ypd03008@gmail.com>
2022-02-11 15:13:41 +0900

    replace test dir

--- jsx-lecture-test/react-shop-test-app/src/mocks/server.js
+++
@@ -1,5 +0,0 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-// mocking server 생성
-export const server = setupServer(...handlers)
--- jsx-lecture-test/react-shop-test-app/src/test-utils.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { OrderContextProvider } from "./contexts/OrderContext";
-
-const customRender = (ui, options) => 
-    render(ui, {wrapper: OrderContextProvider, ...options})
-
-export * from '@testing-library/react'
-
-export {customRender as render}
